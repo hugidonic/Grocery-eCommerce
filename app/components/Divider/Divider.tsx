3ba83382ddb3e3ff5a789ac96ec7411952da0aff
Hugidonic <netvaca@ya.ru>
2022-08-23 14:47:56 +0300

    Delivery address screen created

--- app/components/Divider/Divider.tsx
+++ app/components/Divider/Divider.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Block } from '..'
+import React from 'react';
+import { Block } from '..';
 // Theme
-import { colors } from '../../theme'
+import { colors } from '../../theme';
 
 interface DividerProps {
-  height?: number;
-  color?: string;
+	height?: number;
+	opacity?: number;
+	color?: string;
 }
 
 export const Divider = (props: DividerProps) => {
-  // Default height = 1
-  // Default color = colors.dim
-  const { height = 1, color = colors.dim } = props
-  return (
-    <Block style={{ height, width: '100%', opacity: 0.3 }} color={color} />
-  )
-}
+	// Default height = 1
+	// Default color = colors.dim
+	// Default opacity = 0.3
+	const { height = 1, color = colors.dim, opacity = 0.3 } = props;
+	return <Block style={{ height, width: '100%', opacity: opacity }} color={color} />;
+};
--- app/screens/DeliveryAddress/DeliveryAddressScreen.tsx
+++ app/screens/DeliveryAddress/DeliveryAddressScreen.tsx
@@ -1,34 +1,90 @@
 // React and packages
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { Pressable, StyleSheet } from 'react-native';
 // Navigation styles
 import { StackScreenProps } from '@react-navigation/stack';
 import { ProfileNavigatorParamList } from '../../navigators';
 // import { ParamList } from '../../navigators';
 import { colors, spacing } from '../../theme';
 // Components
-import { Screen, Block, Text } from '../../components';
+import { Screen, Block, Text, Header, RadioButton, Button } from '../../components';
 
 interface DeliveryAddressScreenProps
 	extends StackScreenProps<ProfileNavigatorParamList, 'deliveryAddress'> {}
 
+/**
+ * TODO:
+ *  * MAKE ITEMS "CRUD"able
+ *  * Make various radiobutton sizes
+ *  * Connect with state manager
+ */
+
 export const DeliveryAddressScreen = (props: DeliveryAddressScreenProps) => {
+	const [ activeIdx, setActiveIdx ] = React.useState<number>(0);
+
 	return (
-		<Screen style={[styles.container, styles.center]} preset="scroll">
-			<Text> DeliveryAddress Screen </Text>
+		<Screen style={styles.container} preset="scroll">
+			<Header title="Delivery address" />
+
+			{UsersDeliveryAddress.map((address, idx) => {
+				return (
+					<Block
+						key={idx.toString()}
+						row
+						justify="space-between"
+						marginVertical={spacing[2]}
+					>
+						<Text numberOfLines={1} style={{ width: '85%' }} size="medium">
+							{address.country}, {address.city}, {address.street}, {address.house}
+						</Text>
+						<Pressable onPress={() => setActiveIdx(idx)}>
+							<RadioButton isActive={idx === activeIdx} />
+						</Pressable>
+					</Block>
+				);
+			})}
+
+			<Block
+				align="center"
+				style={{ position: 'absolute', bottom: 40, right: 0, left: 0 }}
+			>
+				<Button shadow text="Add new address" />
+			</Block>
 		</Screen>
 	);
 };
+
+const UsersDeliveryAddress = [
+	{
+		city: 'Kazan',
+		street: 'Sirtlanova',
+		house: '18',
+		country: 'Russia'
+	},
+	{
+		city: 'Ufa',
+		street: 'dfgdfjghlkejdsdrfsdfsdrtlkgtjelr',
+		house: '18',
+		country: 'Russia'
+	},
+	{
+		city: 'Moscow',
+		street: 'Orenbsdfsdgrsd',
+		house: '8',
+		country: 'Russia'
+	},
+	{
+		city: 'Agidel',
+		street: 'Mosdsagk',
+		house: '23',
+		country: 'USA'
+	}
+];
 
 const styles = StyleSheet.create({
 	container: {
 		backgroundColor: colors.palette.offWhite,
 		paddingHorizontal: spacing[5],
-		paddingVertical: spacing[6]
-	},
-	center: {
-		justifyContent: 'center',
-		alignItems: 'center',
-		flex: 1,
+		minHeight: '100%'
 	}
 });
