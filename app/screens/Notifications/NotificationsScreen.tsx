6536b0c72c05dbd61c40d816660cf61716fd75b1
Hugidonic <netvaca@ya.ru>
2022-08-23 15:03:40 +0300

    Notification screen added

--- app/screens/Notifications/NotificationsScreen.tsx
+++ app/screens/Notifications/NotificationsScreen.tsx
@@ -1,41 +1,114 @@
 // React and packages
 import React from 'react';
 import { StackScreenProps } from '@react-navigation/stack';
-import { StyleSheet } from 'react-native'
+import { Pressable, StyleSheet } from 'react-native';
 // Types and utils
 // import { ParamList } from '../../navigators';
 import { colors, spacing } from '../../theme';
 // Components
-import {
-	Screen,
-	Block,
-	Text,
-	Loading,
-} from '../../components';
+import { Screen, Block, Text, Header } from '../../components';
 import { ProfileNavigatorParamList } from '../../navigators';
+import uuid from '../../utils/uuid';
+import Entypo from 'react-native-vector-icons/Entypo';
 
-interface NotificationsScreenProps extends StackScreenProps<ProfileNavigatorParamList, 'notifications'> {
-
-}
+interface NotificationsScreenProps
+	extends StackScreenProps<ProfileNavigatorParamList, 'notifications'> {}
 
 export const NotificationsScreen = (props: NotificationsScreenProps) => {
 
-  return (
-    <Screen style={[styles.container, styles.center]} preset="scroll">
-			<Text> Notifications Screen </Text>
-    </Screen>
-  )
-}
+	const notifyTypes = {
+		"FIRE": "🔥",
+		"QUESTION": "🤨",
+		"SALE": "🤑"
+	}
+
+	/**
+	 * TODO:
+	 *  * MAKE DELETE ANIMATION
+	 */
+	
+	return (
+		<Screen style={styles.container} preset="scroll">
+			<Header title="Notifications" />
+
+			
+			{
+				UsersNotifications.map((notify, idx) => {
+					return (
+						<Block key={notify.id} shadow style={styles.card} marginVertical={10} row>
+							{/* ICON */}
+							<Block justify='center' align='center' style={{width: '15%'}}>
+								<Text size="title">{notifyTypes[notify.type]}</Text>
+							</Block>
+							{/* TEXT */}
+							<Block flex>
+								<Text  numberOfLines={3} >{notify.text}</Text>
+							</Block>
+							{/* CHEVRON */}
+							<Pressable style={{ width: '15%', justifyContent: 'center', alignItems: 'center' }}>
+								<Entypo name="chevron-right" size={30} color={colors.palette.black} />
+							</Pressable>
+						</Block>
+					)
+				})
+			}
+			
+		</Screen>
+	);
+};
 
 const styles = StyleSheet.create({
 	container: {
 		backgroundColor: colors.palette.offWhite,
 		paddingHorizontal: spacing[5],
-		paddingVertical: spacing[6]
+		minHeight: '100%'
 	},
-	center: {
-		justifyContent: 'center',
-		alignItems: 'center',
-		flex: 1,
+	card: {
+		backgroundColor: colors.palette.white,
+		borderRadius: 14,
+		paddingVertical: spacing[2],
+		paddingHorizontal: spacing[1],
 	}
 });
+
+const UsersNotifications = [
+	{
+		id: uuid(),
+		text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec feugiat in fermentum posuere urna nec tincidunt praesent. Morbi tempus iaculis urna id volutpat.`,
+		type: 'FIRE',
+	},
+	{
+		id: uuid(),
+		text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec feugiat in fermentum posuere urna nec tincidunt praesent. Morbi tempus iaculis urna id volutpat.`,
+		type: 'SALE',
+	},
+	{
+		id: uuid(),
+		text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec feugiat in fermentum posuere urna nec tincidunt praesent. Morbi tempus iaculis urna id volutpat.`,
+		type: 'QUESTION',
+	},
+	{
+		id: uuid(),
+		text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec feugiat in fermentum posuere urna nec tincidunt praesent. Morbi tempus iaculis urna id volutpat.`,
+		type: 'SALE',
+	},	{
+		id: uuid(),
+		text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec feugiat in fermentum posuere urna nec tincidunt praesent. Morbi tempus iaculis urna id volutpat.`,
+		type: 'QUESTION',
+	},	{
+		id: uuid(),
+		text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec feugiat in fermentum posuere urna nec tincidunt praesent. Morbi tempus iaculis urna id volutpat.`,
+		type: 'FIRE',
+	},	{
+		id: uuid(),
+		text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec feugiat in fermentum posuere urna nec tincidunt praesent. Morbi tempus iaculis urna id volutpat.`,
+		type: 'SALE',
+	},
+	{
+		id: uuid(),
+		text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec feugiat in fermentum posuere urna nec tincidunt praesent. Morbi tempus iaculis urna id volutpat.`,
+		type: 'FIRE',
+	},
+];
+
+
