a4c449c3d51c79d96247400358456e48f9614a05
Hugidonic <netvaca@ya.ru>
2022-08-28 16:07:33 +0300

    Profile start logic created

--- app/modules/Profile/profile.actionTypes.ts
+++ app/modules/Profile/profile.actionTypes.ts
@@ -1,49 +1,3 @@
-import { DeliveryAddressType, PromoCardType } from './profile.types';
-
-// Delivery addresses
-export enum DeliveryAddressTypes {
-	PICK_ADDRESS = 'PICK_ADDRESS',
-	CREATE_ADDRESS = 'CREATE_ADDRESS',
-	DELETE_ADDRESS = 'DELETE_ADDRESS',
-	EDIT_ADDRESS = 'EDIT_ADDRESS'
-}
-interface PickAddressAction {
-	type: DeliveryAddressTypes.PICK_ADDRESS;
-	payload: DeliveryAddressType['id'];
-}
-interface CreateAddressAction {
-	type: DeliveryAddressTypes.CREATE_ADDRESS;
-	payload: DeliveryAddressType;
-}
-interface EditAddressAction {
-	type: DeliveryAddressTypes.EDIT_ADDRESS;
-	payload: DeliveryAddressType;
-}
-interface DeleteAddressAction {
-	type: DeliveryAddressTypes.DELETE_ADDRESS;
-	payload: DeliveryAddressType['id'];
-}
-export type DeliveryAddressActions =
-	| PickAddressAction
-	| CreateAddressAction
-	| EditAddressAction
-	| DeleteAddressAction;
-
-// PROMO
-export enum PromoCardTypes {
-	ADD_PROMO_CARD = 'ADD_PROMO_CARD',
-	PICK_PROMO_CARD = 'PICK_PROMO_CARD'
-}
-interface AddPromoCardAction {
-	type: PromoCardTypes.ADD_PROMO_CARD;
-	payload: PromoCardType;
-}
-interface PickPromoCardAction {
-	type: PromoCardTypes.ADD_PROMO_CARD;
-	payload: PromoCardType['id'];
-}
-export type PromoCardActions = AddPromoCardAction | PickPromoCardAction;
-
 // DETAILS
 export enum DetailsTypes {
 	SET_FIRST_NAME = 'SET_FIRST_NAME',
@@ -63,4 +17,5 @@ interface SetEmailAction {
 	payload: string;
 }
 
-export type DetailsActions = SetFirstNameAction | SetLastNameAction | SetEmailAction;
+export type ProfileActions = SetFirstNameAction | SetLastNameAction | SetEmailAction;
+
--- app/modules/Profile/profile.types.ts
+++ app/modules/Profile/profile.types.ts
@@ -1,39 +1,19 @@
-import { OrderItemType } from '../Orders';
 
 /* TYPES */
-export type ProfileType = {
-	orders: OrderItemType[];
-	details: UserDetailsType;
-	deliveryAddresses: DeliveryAddressType[];
-	paymentMethods: PaymentMethodType[];
-	promoCards: PromoCardType[];
-	notifications: NotificationType[];
-};
+// export type ProfileType = {
+// 	orders: OrderItemType[];
+// 	details: UserDetailsType;
+// 	deliveryAddresses: DeliveryAddressType[];
+// 	paymentMethods: PaymentMethodType[];
+// 	promoCards: PromoCardType[];
+// 	notifications: NotificationType[];
+// };
+
+export type ProfileStateType = {
+	UserDetails: UserDetailsType;
+}
 export type UserDetailsType = {
 	firstName: string;
 	secondName: string;
 	email: string;
-};
-export type DeliveryAddressType = {
-	id: string;
-	city: string;
-	street: string;
-	house: string;
-	country: string;
-};
-export type PaymentMethodType = {
-	last4digits: string;
-	color: string;
-	cardName: 'MasterCard' | 'VISA' | 'MIR' | 'MAESTRO';
-};
-export type PromoCardType = {
-	id: string;
-	beginDate: string;
-	endDate: string;
-	price: number;
-};
-export type NotificationType = {
-	id: string;
-	text: string;
-	type: 'FIRE' | 'SALE' | 'QUESTION';
 };
